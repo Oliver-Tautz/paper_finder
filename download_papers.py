610da004e822f75be24e9890d13d8b490b0f725b
Oliver Tautz <oliver.tautz@uni-bielefeld.de>
2022-03-03 15:44:46 +0100

    add sleep command to not get locked out by google

--- download_papers.py
+++ download_papers.py
@@ -3,6 +3,7 @@ import subprocess
 from os import makedirs,listdir
 from utils import papers_read_from_csv,papers_write_to_csv
 import re
+import time 
 
 CSV_NAME = "papers.csv"
 OUTPUT_DIR =  "downloaded_papers"
@@ -17,6 +18,7 @@ makedirs(OUTPUT_DIR,exist_ok=True)
 for i,p in enumerate(papers):
     if not p.filename:
         process = subprocess.run(['sopaper',f'\"{p.title}\"','-d',OUTPUT_DIR])
+        time.sleep(60)
     downloaded_names =  listdir(OUTPUT_DIR)   
     for name in downloaded_names:
         if re.search(fr'"?{p.title}"?\.pdf',name,re.IGNORECASE):
