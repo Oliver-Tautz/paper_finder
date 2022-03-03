28cbf717ac6d399e1ccb1d1fa98285693d7ccc20
Oliver Tautz <oliver.tautz@uni-bielefeld.de>
2022-03-03 15:38:40 +0100

    remove debug code

--- download_papers.py
+++ download_papers.py
@@ -4,22 +4,22 @@ from os import makedirs,listdir
 from utils import papers_read_from_csv,papers_write_to_csv
 import re
 
-INPUT_CSV_NAME = "papers.csv"
+CSV_NAME = "papers.csv"
 OUTPUT_CSV_NAME = "papers_downloaded.csv"
-OUTPUT_DIR =  "downloaded_papers"
 
 
-papers = papers_read_from_csv(INPUT_CSV_NAME)
-#papers.reverse()
+papers = papers_read_from_csv(CSV_NAME)
+papers.reverse()
 
 makedirs(OUTPUT_DIR,exist_ok=True)
 
 
-for i,p in enumerate(papers[0:10]):
+for i,p in enumerate(papers):
     if not p.filename:
         process = subprocess.run(['sopaper',f'\"{p.title}\"','-d',OUTPUT_DIR])
     downloaded_names =  listdir(OUTPUT_DIR)   
     for name in downloaded_names:
         if re.search(fr'"?{p.title}"?\.pdf',name,re.IGNORECASE):
             papers[i].filename = name
-papers_write_to_csv(papers,OUTPUT_CSV_NAME)
+
+papers_write_to_csv(papers,CSV_NAME)
