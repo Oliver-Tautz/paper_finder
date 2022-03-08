464364ccc3d98224641cacc15e9b70e370a97779
Oliver Tautz <oliver.tautz@uni-bielefeld.de>
2022-03-08 14:31:12 +0100

    download all not present as files, ignore csv

--- download_papers.py
+++ download_papers.py
@@ -11,24 +11,28 @@ OUTPUT_DIR =  "downloaded_papers"
 
 
 papers = papers_read_from_csv(CSV_NAME)
-papers.reverse()
+#papers.reverse()
 
 makedirs(OUTPUT_DIR,exist_ok=True)
 
 
 for i,p in enumerate(tqdm(papers)):
-    if not p.filename or p.filename=="not found":
-        process = subprocess.run(['sopaper',f'\"{p.title}\"','-d',OUTPUT_DIR])
-        time.sleep(60)
-    downloaded_names =  listdir(OUTPUT_DIR)   
+
+    
     found = False
+    downloaded_names =  listdir(OUTPUT_DIR)   
     for name in downloaded_names:
         if re.search(fr'"?{p.title}"?\.pdf',name,re.IGNORECASE):
             papers[i].filename = name
             found = True
+    
+
+
     if not found:
+        process = subprocess.run(['sopaper',f'\"{p.title}\"','-d',OUTPUT_DIR])
+        time.sleep(60)
         papers[i].filename='not found'
 
 
-papers.reverse()
+#papers.reverse()
 papers_write_to_csv(papers,CSV_NAME)
